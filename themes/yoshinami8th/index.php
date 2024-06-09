945903ec22bb610e5d0b2d38dad934045c7ec3f2
rotelstift <rotelstift@gmail.com>
2024-06-09 23:58:28 +0900

    色々微調整

--- themes/yoshinami8th/index.php
+++ themes/yoshinami8th/index.php
@@ -12,7 +12,7 @@
   <div class="w-full min-h-screen flex flex-col bg-neutral-300 text-neutral-700">
     <header class="mb-8">
         <div class="container px-4 mx-auto lg:w-4/5 lg:max-w-screen-md">
-            <h1 class="text-4xl lg:text-6xl"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+            <h1 class="text-4xl md:text-6xl"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
             <nav class="text-right">
                 <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
                 <?php echo do_shortcode('[tag_menu]'); ?>
@@ -21,28 +21,28 @@
     </header>
 
     <main class="flex-grow">
-        <div class="container grid gap-8 px-4 mx-auto lg:w-4/5 lg:max-w-screen-md">
+        <div class="container grid gap-4 divide-dotted divide-neutral-700 divide-y px-4 mx-auto lg:w-4/5 lg:max-w-screen-md">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article <?php post_class(); ?>>
-                    <h2 class="text-3xl"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <h2 class="text-3xl my-4"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="post-thumbnail">
                         <?php if (has_post_thumbnail()) : ?>
-                          <div class="w-fit mx-auto">
+                          <div class="w-fit max-w-96 mx-auto">
                             <a href="<?php the_permalink(); ?>">
-                                <?php the_post_thumbnail('large'); ?>
+                                <?php the_post_thumbnail('medium_large'); ?>
                             </a>
                           </div>
                         <?php endif; ?>
                     </div>
-                    <div class="post-meta">
+                    <div class="post-content my-6 grid gap-2">
+                        <?php the_content(); ?>
+                    </div>
+                    <div class="post-meta text-right">
                         <span><?php the_time('m-d Y'); ?></span> |
                         <span><?php the_author(); ?></span>
                     </div>
-                    <div class="post-tags">
+                    <div class="post-tags text-right">
                         <?php the_tags('<span>Section: </span>', ', ', ''); ?>
-                    </div>
-                    <div class="post-content mt-8 grid gap-2">
-                        <?php the_content(); ?>
                     </div>
                 </article>
             <?php endwhile; ?>
