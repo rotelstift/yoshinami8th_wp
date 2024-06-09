b84a34c205842bc8dd23823367ea6b7a9ace8ed1
rotelstift <rotelstift@gmail.com>
2024-06-09 23:25:46 +0900

    センタリングの当て方を直す

--- themes/yoshinami8th/index.php
+++ themes/yoshinami8th/index.php
@@ -11,7 +11,7 @@
 <body <?php body_class(); ?>>
   <div class="w-full min-h-screen flex flex-col bg-neutral-300 text-neutral-700">
     <header class="mb-8">
-        <div class="container px-4 lg:w-4/5 lg:max-w-screen-md lg:mx-auto">
+        <div class="container px-4 mx-auto lg:w-4/5 lg:max-w-screen-md">
             <h1 class="text-4xl lg:text-6xl"><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
             <nav class="text-right">
                 <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
@@ -21,7 +21,7 @@
     </header>
 
     <main class="flex-grow">
-        <div class="container grid gap-8 px-4 lg:w-4/5 lg:max-w-screen-md lg:mx-auto">
+        <div class="container grid gap-8 px-4 mx-auto lg:w-4/5 lg:max-w-screen-md">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
                 <article <?php post_class(); ?>>
                     <h2 class="text-3xl"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -34,14 +34,14 @@
                           </div>
                         <?php endif; ?>
                     </div>
-                    <div class="post-meta mb-8">
+                    <div class="post-meta">
                         <span><?php the_time('m-d Y'); ?></span> |
                         <span><?php the_author(); ?></span>
                     </div>
                     <div class="post-tags">
                         <?php the_tags('<span>Section: </span>', ', ', ''); ?>
                     </div>
-                    <div class="post-content grid gap-4">
+                    <div class="post-content mt-8 grid gap-2">
                         <?php the_content(); ?>
                     </div>
                 </article>
@@ -62,7 +62,7 @@
     </main>
 
     <footer>
-        <div class="container px-4 text-center lg:w-4/5 lg:max-w-screen-md lg:mx-auto">
+        <div class="container px-4 text-center mx-auto lg:w-4/5 lg:max-w-screen-md">
           <div class="grid grid-cols-3 gap-8 content-between">
             <p><a href="https://www.pixiv.net/users/3864757">pixiv</a></p>
             <p><a href="https://x.com/yoshinami8th">X</a></p>
